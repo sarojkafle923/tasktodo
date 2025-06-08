c8370cb7d84cc4a6771d058f956c419497999196
Kritish Sharma <sharma.kritish911@gmail.com>
2025-06-08 12:18:46 +1000

    Migration and Model Created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField()
+    isCompleted = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
