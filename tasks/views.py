1db35f85d74e5e734d0d825341b17a8e1c8d2c7a
Manish Kafle <manishkafle385@gmail.com>
2025-06-08 19:31:44 +1000

    Structured response

--- tasks/views.py
+++ tasks/views.py
@@ -17,4 +17,5 @@ def list_tasks(request):
         response_text += f"- {task.name}: {task.description}\n"
         response_text += f"- {task.created.strftime("%d-%m-%Y %I:%M %p")}\n"
         response_text += f"- {'Task Completed' if task.isCompleted else 'Not Done'}"
+        response_text += "\n_______________________________________________________\n"
     return HttpResponse(response_text, content_type='text/plain')
