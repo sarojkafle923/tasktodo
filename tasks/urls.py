a32dc0072bc50b8b3dd33a2e56a6e1b67ff74566
Saroj Kafle <64567189+sarojkafle923@users.noreply.github.com>
2025-06-09 00:36:30 +1000

    Update model and added migrations (#9)
    * update model and added migrations
    
    * update register page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from tasks import views
 
 urlpatterns = [
-    path('', views.list_task)
+    path('', views.list_task, name='tasks'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
