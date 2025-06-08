ee85f2afda68f56e12d4323a807385dba1ee4d16
Saroj Kafle <sarojkafle5@gmail.com>
2025-06-08 17:02:35 +1000

    update database to include foregin key

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+# ------ Authentication Views ------
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'tasks/register.html'
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
