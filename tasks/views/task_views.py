19022c692c295b8069db651182579efb652bfb1e
Saroj Kafle <sarojkafle5@gmail.com>
2025-06-14 15:19:19 +1000

    avoid early call to database

--- tasks/views/task_views.py
+++ tasks/views/task_views.py
@@ -6,17 +6,9 @@ from tasks.constants import RouteGroup
 from tasks.models import Task
 
 class ListTaskView(LoginRequiredMixin, View):
-    tasks = Task.objects.all()
-
-    # Build a plain text response from tasks
-    response_text = "Tasks List:\n"
-    for task in tasks:
-        response_text += f"- {task.title}: {task.description}\n"
-        response_text += f"- {task.created_at.strftime("%d-%m-%Y %I:%M %p")}\n"
-        response_text += f"- {'Task Completed' if task.completed else 'Not Done'}"
-        response_text += "\n_______________________________________________________\n"
-
     @staticmethod
     def get(request):
         """Render the tasks page"""
+        tasks = Task.objects.filter(user_id=request.user.id)
+        print(tasks)
         return render(request, RouteGroup.PROTECTED.TASKS.LIST.template_path)
